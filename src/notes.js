358f0ba693e2680c9edb767cc2c1d207f4e69ea5
AndikaPrasetia <prasetyadika2502@gmail.com>
2023-06-07 02:41:44 +0700

    restructure with hapi plugin

--- src/notes.js
+++
@@ -1,8 +0,0 @@
-/**
- * Notes: membuat data notes yang di simpan dalam bentuk array object.
- */
-
-// array untuk menampung objek catatan.
-const notes = [];
-
-module.exports = notes;
--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@
  * dan menjalankan server HTTP menggunakan Hapi.
  */
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+// const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 // membuat server menggunakan Hapi
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -21,7 +23,14 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  // server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
